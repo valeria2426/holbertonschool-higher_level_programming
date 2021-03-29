195ef9b8991522bba0de20986f497c984f62eb1a
valeria2426 <vtrujillorodriguez70@gmail.com>
2021-03-28 20:46:36 -0500

    my Poyects objets relational mapping

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,11 +1,5 @@
-#/usr/bin/python3
-# Write a file that contains class defns os a state and an instance
-# Base =  delcarative_base()
-# State class:
-# inherits from base class, link to MySQL table 'state',
-# has column id and name
-# Must use SQLAlchemy
-from sqlalchemy import Column, Integer, String
+#!/usr/bin/python3
+from sqlalchemy import Column, String, Integer
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -14,5 +8,5 @@ Base = declarative_base()
 class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True,
-                nullable=False, autoincrement=True)
+                autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
